5f269e235bd72d1bfa5070029cea15e2e839eed1
Vishwajeet Kumar <100605183+thevk70@users.noreply.github.com>
2023-02-02 19:52:57 +0530

    Add files via upload

---
+++ Assign23.cpp
@@ -0,0 +1,128 @@
+// 1. Write a C++ program to print Hello iNeuron on the screen.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"Hello iNeuron";
+//     return 0;
+// }
+
+// 2. Write a C++ program to print Hello on the first line and iNeuron on the second line using 
+// endl.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"Hello"<<endl<<"iNeuron";
+//     return 0;
+// }
+
+// 3. Write a C++ program to calculate the sum of two numbers.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout<<"Enter two numbers::"<<endl;
+//     cin>>a>>b;
+//     int sum = a+b;
+//     cout<<"Sum = "<<sum;
+//     return 0;
+// }
+
+// 4. Write a C++ program to calculate the area of a circle
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     float r,area;
+//     cout<<"Enter radius of circle::"<<endl;
+//     cin>>r;
+//     area = 3.14*r*r;
+//     cout<<"Area of circle = "<<area;
+//     return 0;
+// }
+
+// 5. Write a C++ program to calculate the volume of a cuboid.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     float s,area;
+//     cout<<"Enter side of cuboid::"<<endl;
+//     cin>>s;
+//     area = s*s*s;
+//     cout<<"Area of cuboid = "<<area;
+//     return 0;
+// }
+
+// 6. Write a C++ program to calculate an average of 3 numbers.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b,c,avg;
+//     cout<<"Enter three numbers::"<<endl;
+//     cin>>a>>b>>c;
+//     avg = (a+b+c)/3;
+//     cout<<"Average = "<<avg;
+//     return 0;
+// }
+
+// 7. Write a C++ program to calculate the square of a number
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout<<"Enter a number::"<<endl;
+//     cin>>n;
+//     int sqr = n*n;
+//     cout<<"Square of "<<n<<" is "<<sqr<<".";
+//     return 0;
+// }
+
+// 8. Write a C++ program to swap values of two int variables without using third variable
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 4,b = 2;
+//     cout<<"Before swap::"<<endl;
+//     cout<<"a = "<<a<<" b = "<<b<<endl;
+//     a = a+b;
+//     b = a-b;
+//     a = a-b;
+//     cout<<"After swap::"<<endl;
+//     cout<<"a = "<<a<<" b = "<<b<<endl;
+//     return 0;
+// }
+
+// 9. Write a C++ program to find the maximum of two numbers.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout<<"Enter two numbers::"<<endl;
+//     cin>>a>>b;
+//     (a>b) ? cout<<a<<" is greater." : cout<<b<<" is greater.";
+//     return 0;
+// }
+
+// 10. Write a C++ program to add all the numbers of an array of size 10.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a[10],sum=0;
+//     cout<<"Enter all 10 element of array::"<<endl;
+//     for (int i = 0; i < 10; i++)
+//     {
+//         cin>>a[i];
+//         sum+=a[i];
+//     }
+//     cout<<"Sum of all numbers of array is "<<sum<<".";
+//     return 0;
+// }
+
